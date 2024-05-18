91ac640e268d96f6fe162257f1167764703ba68e
Sridhar <113336066+Sridharsaravana@users.noreply.github.com>
2024-05-18 14:02:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,9 @@
 
 #If  the branch is dev, build the image and push it to docker-hub dev repository
 
-if [[ $GIT_BRANCH == "origin/dev" ]]; then
     echo "Building image for dev branch ... "
     docker build -t reactapp  .
     echo $DOCKERHUB_PSW | docker login -u  $DOCKERHUB_USR --password-stdin
     docker tag reactapp sridharsaravana/dev
     docker push sridharsaravana/dev:latest
 
-
-elif [[ $GIT_BRANCH == "origin/prod" ]]; then
-      echo "Building image for prod branch ..."
-      docker build -t reactapp .
-      echo $DOCKERHUB_PSW | docker login -u  $DOCKERHUB_USR --password-stdin
-      docker tag reactapp sridharsaravana/prod
-      docker push sridharsaravana/prod:latest
-fi
