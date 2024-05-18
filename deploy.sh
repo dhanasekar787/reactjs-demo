4a5df2ed76dd081078bd8f043a88defc45429bbf
Sridhar <113336066+Sridharsaravana@users.noreply.github.com>
2024-05-18 13:22:29 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ IMAGENAME="reactapp"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
   #If  the branch is dev, build the image and push it to docker-hub dev repository
 
-if [ [$GIT_BRANCH == "origin/dev" ]]; then
+if [ "$BRANCH" == "dev" ]]; then
     echo "Building image for dev branch ... "
     docker build -t $IMAGENAME .
-    echo $DOCKERHUB_CREDENTIALS_Sridhar@98 | docker login -u  $DOCKERHUB_CREDENTIALS_sridahrsaravana --password-stdin
+    echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u  $DOCKERHUB_CREDENTIALS_USR --password-stdin
     docker tag reactapp sridharsaravana/dev
     docker push sridharsaravana/dev:latest
     docker logout
