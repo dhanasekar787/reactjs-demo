00b685a44da451b8293ba36151662ec551610c30
Sridhar <113336066+Sridharsaravana@users.noreply.github.com>
2024-05-18 14:33:08 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#set the image
 
-#If  the branch is dev, build the image and push it to docker-hub dev repository
-
+#Deploy script for dev branch
     echo "Building image for dev branch ... "
     docker build -t reactapp  .
     docker login -u sridharsaravana -p dckr_pat_fm9zwkuBm0qOstqPurVhQss74zg
