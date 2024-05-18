64dc6dd9eab5c12b714853d21ee58170079f01c6
Sridhar <113336066+Sridharsaravana@users.noreply.github.com>
2024-05-18 14:14:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 
     echo "Building image for dev branch ... "
     docker build -t reactapp  .
-    docker login -u sridharsarvana -p dckr_pat_fm9zwkuBm0qOstqPurVhQss74zg
+    docker login -u sridharsaravana -p dckr_pat_fm9zwkuBm0qOstqPurVhQss74zg
     docker tag reactapp sridharsaravana/dev
     docker push sridharsaravana/dev:latest
 
