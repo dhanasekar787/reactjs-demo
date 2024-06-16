328f6f783019f2197cbf4b87c8f0d58f6fcc11e0
dhanasekar787 <dhanasekarvce@gmail.com>
2024-06-16 23:03:17 +0530

    New Files

---
+++ src/Deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Deploy script for dev branch
+    echo "Building image for dev branch ... "
+    docker build -t reactapp  .
+    docker login -u username -p dokcer_token
+    docker tag reactapp sridharsaravana/dev
+    docker push sridharsaravana/dev:latest
