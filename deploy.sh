9573070feb62a74cc37982cf30b280baecb7e8fa
Dhanasekar <134762764+dhanasekar787@users.noreply.github.com>
2024-06-18 23:03:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@
     echo "Building image for dev branch ... "
     docker build -t reactapp  .
     docker login -u username -p dokcer_token
-    docker tag reactapp sridharsaravana/dev
-    docker push sridharsaravana/dev:latest
+    docker tag reactapp dhanadev/dev
+    docker push dhanadev/dev:latest
 
